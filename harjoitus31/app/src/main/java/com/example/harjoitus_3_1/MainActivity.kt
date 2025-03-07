51783455600bf43f16a7c584daaca7d03c5d6a05
jonne <jonne.vuorela@proton.me>
2025-03-07 11:19:20 +0200

    harjoitus-3-1 done

--- harjoitus31/app/src/main/java/com/example/harjoitus_3_1/MainActivity.kt
+++ harjoitus31/app/src/main/java/com/example/harjoitus_3_1/MainActivity.kt
@@ -3,45 +3,38 @@ package com.example.harjoitus_3_1
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     lateinit var sharedPref: SharedPreferences
 
-    lateinit var button1Koti: Button
-    lateinit var button2Koti: Button
-    lateinit var button3Koti: Button
-    lateinit var buttonVirheKoti: Button
+    var virheetKotiKey: String = "virheet_koti"
+    var pisteetKotiKey: String = "pisteet_koti"
     lateinit var virheetKoti: TextView
-    lateinit var pisteenKoti: TextView
+    lateinit var pisteetKoti: TextView
 
-    lateinit var button1Vieras: Button
-    lateinit var button2Vieras: Button
-    lateinit var button3Vieras: Button
-    lateinit var buttonVirheVieras: Button
+    var virheetVierasKey: String = "virheet_vieras"
+    var pisteetVierasKey: String = "pisteet_vieras"
     lateinit var virheetVieras: TextView
     lateinit var pisteetVieras: TextView
 
     fun initUi(){
-        button1Koti = findViewById(R.id.button1_koti)
-        button2Koti = findViewById(R.id.button2_koti)
-        button3Koti = findViewById(R.id.button3_koti)
-        buttonVirheKoti = findViewById(R.id.button_virhe_koti)
-
-        button1Vieras = findViewById(R.id.button1_vieras)
-        button2Vieras = findViewById(R.id.button2_vieras)
-        button3Vieras = findViewById(R.id.button3_vieras)
-        buttonVirheVieras = findViewById(R.id.button_virhe_vieras)
-
         virheetKoti = findViewById(R.id.virheet_koti)
         virheetVieras = findViewById(R.id.virheet_vieras)
-        pisteenKoti = findViewById(R.id.pisteet_koti)
+        pisteetKoti = findViewById(R.id.pisteet_koti)
         pisteetVieras = findViewById(R.id.pisteet_vieras)
+
+        virheetKoti.text = sharedPref.getInt(virheetKotiKey, 0).toString()
+        virheetVieras.text = sharedPref.getInt(virheetVierasKey, 0).toString()
+        pisteetKoti.text = sharedPref.getInt(pisteetKotiKey, 0).toString()
+        pisteetVieras.text = sharedPref.getInt(pisteetVierasKey, 0).toString()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,5 +51,68 @@ class MainActivity : AppCompatActivity() {
         initUi()
     }
 
+    // Virhe laskut
+    fun addVirheVieras(view: View){
+        val editor = sharedPref.edit()
+        val virheet = virheetVieras.text.toString().toInt() + 1
+
+        virheetVieras.text = virheet.toString()
+        editor.putInt(virheetVierasKey, virheet)
+        editor.apply()
+    }
+    fun reduceVirheVieras(view: View){
+        val editor = sharedPref.edit()
+        val virheet = virheetVieras.text.toString().toInt() - 1
+
+        virheetVieras.text = virheet.toString()
+        editor.putInt(virheetVierasKey, virheet)
+        editor.apply()
+    }
+    fun addVirheKoti(view: View){
+        val editor = sharedPref.edit()
+        val virheet = virheetKoti.text.toString().toInt() + 1
+
+        virheetKoti.text = virheet.toString()
+        editor.putInt(virheetKotiKey, virheet)
+        editor.apply()
+    }
+    fun reduceVirheKoti(view: View){
+        val editor = sharedPref.edit()
+        val virheet = virheetKoti.text.toString().toInt() - 1
+
+        virheetKoti.text = virheet.toString()
+        editor.putInt(virheetKotiKey, virheet)
+        editor.apply()
+    }
+
+    // Piste laskut
+    fun pisteetKoti(view: View){
+        val editor = sharedPref.edit()
+        var pisteet = pisteetKoti.text.toString().toInt()
+        when (view.id){
+            R.id.pisteet_koti -> pisteet-=1
+            R.id.button1_koti -> pisteet+=1
+            R.id.button2_koti -> pisteet+=2
+            R.id.button3_koti -> pisteet+=3
+        }
+        pisteetKoti.text = pisteet.toString()
+        editor.putInt(pisteetKotiKey, pisteet)
+        editor.apply()
+
+    }
+    fun pisteetVieras(view: View){
+        val editor = sharedPref.edit()
+        var pisteet = pisteetVieras.text.toString().toInt()
+        when (view.id){
+            R.id.pisteet_vieras -> pisteet-=1
+            R.id.button1_vieras -> pisteet+=1
+            R.id.button2_vieras -> pisteet+=2
+            R.id.button3_vieras -> pisteet+=3
+        }
+        pisteetVieras.text = pisteet.toString()
+        editor.putInt(pisteetVierasKey, pisteet)
+        editor.apply()
+
+    }
 
 }
