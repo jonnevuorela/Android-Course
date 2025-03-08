582e3025bf4a8ad71042ff64fe8f224ad281a329
jonne <jonne.vuorela@proton.me>
2025-03-08 14:18:30 +0200

    noppapelin gameinstance logiikka/harjoitus-3-2 done

--- harjoitus32/app/src/main/java/com/example/harjoitus_3_2/MainActivity.kt
+++ harjoitus32/app/src/main/java/com/example/harjoitus_3_2/MainActivity.kt
@@ -2,27 +2,42 @@ package com.example.harjoitus_3_2
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.hardware.camera2.CameraExtensionSession.StillCaptureLatency
 import android.os.Bundle
 import android.preference.Preference
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var sharedPref: SharedPreferences
 
+    var gameInstanceKey: String = "game_instance"
+    var gameInstance: GameInstance? = null
+
     lateinit var dice1: ImageView
     lateinit var dice2: ImageView
+
+    lateinit var rollBtn: Button
+    lateinit var startBtn: Button
+    lateinit var scoreText: TextView
 
 
     fun initUi(){
         dice1 = findViewById(R.id.dice1)
         dice2 = findViewById(R.id.dice2)
+        rollBtn = findViewById(R.id.roll)
+        startBtn = findViewById(R.id.start)
+        scoreText = findViewById(R.id.pisteet)
         updateDice()
     }
 
@@ -35,21 +50,88 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+
+        val gameInstanceJson = sharedPref.getString(gameInstanceKey, null)
         initUi()
+        if (gameInstanceJson != null) {
+            try {
+                val jsonObject = JSONObject(gameInstanceJson)
+                gameInstance = GameInstance(
+                    score = jsonObject.getInt("score"),
+                    rollsLeft = jsonObject.getInt("rollsLeft"),
+                    isGameOver = jsonObject.getBoolean("isGameOver")
+                )
+            } catch (e: Exception) {
+                Toast.makeText(this, "virhe pelitietojen hakemisessa, alustetaan uusi peli.", Toast.LENGTH_LONG).show()
+            }
+        }
+        refresh()
+    }
+    fun refresh() {
+        if (gameInstance != null) {
+            gameInstance?.let {
+                scoreText.text = "${it.score}"
+
+                rollBtn.isEnabled = !it.isGameOver && it.rollsLeft > 0
+                startBtn.isEnabled = it.isGameOver || it.rollsLeft <= 0
+            }
+        } else {
+            scoreText.text = "0"
+            rollBtn.isEnabled = false
+            startBtn.isEnabled = true
+        }
+
+        updateDice()
+    }
+    fun saveGameInstance(){
+        val editor = sharedPref.edit()
+        val gameInstanceJson = JSONObject()
+        gameInstance?.let {
+            gameInstanceJson.put("score", it.score)
+            gameInstanceJson.put("rollsLeft", it.rollsLeft)
+            gameInstanceJson.put("isGameOver", it.isGameOver)
+        }
+
+        editor.putString(gameInstanceKey, gameInstanceJson.toString())
+        editor.apply()
+    }
+    fun newGameInstance(view: View){
+        gameInstance = GameInstance(
+            score = 0,
+            rollsLeft = 5,
+            isGameOver = false,
+        )
+        saveGameInstance()
+        refresh()
 
     }
 
     fun roll(view: View){
-        val number1: Int = Random.nextInt(0,6)
-        val number2: Int = Random.nextInt(0,6)
-        val editor = sharedPref.edit()
+        gameInstance?.let {
+            if (it.rollsLeft > 0 && !it.isGameOver) {
+                val number1: Int = Random.nextInt(0, 6)
+                val number2: Int = Random.nextInt(0, 6)
+                val editor = sharedPref.edit()
 
-        editor.putInt("dice1", number1)
-        editor.putInt("dice2", number2)
-        editor.apply()
-        updateDice()
+                editor.putInt("dice1", number1)
+                editor.putInt("dice2", number2)
+                editor.apply()
+
+                it.score += (number1 + number2 + 2)
+                it.rollsLeft -= 1
+
+                if (it.rollsLeft <= 0) {
+                    it.isGameOver = true
+
+                }
+                updateDice()
+                saveGameInstance()
+                refresh()
+            }
+        }
+
+
     }
 
     fun updateDice(){
@@ -62,6 +144,12 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+data class GameInstance(
+    var score: Int,
+    var rollsLeft: Int,
+    var isGameOver: Boolean,
+)
+
 enum class DiceDrawable(val drawableResId: Int) {
     dice_1(R.drawable.dice_1),
     dice_2(R.drawable.dice_2),
@@ -72,7 +160,7 @@ enum class DiceDrawable(val drawableResId: Int) {
 
     companion object {
         fun fromIndex(index: Int): DiceDrawable {
-            return values()[index]
+            return entries[index]
         }
     }
 }
