ee833c22d13d5bbda9b161c800fe16550dfc843b
jonne <jonne.vuorela@proton.me>
2025-03-13 21:32:42 +0200

    progress

--- harjoitus35/app/src/main/java/com/example/harjoitus_3_5/YhteenvetoFragment.kt
+++ harjoitus35/app/src/main/java/com/example/harjoitus_3_5/YhteenvetoFragment.kt
@@ -5,11 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,11 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [StartFragment.newInstance] factory method to
+ * Use the [YhteenvetoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class StartFragment : Fragment() {
-
+class YhteenvetoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,41 +31,22 @@ class StartFragment : Fragment() {
         }
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_start, container, false)
-
-        val spinner: Spinner = view.findViewById(R.id.spinner)
-        val items = listOf("Valitse Toiminto", "Päivärahat", "Kilometrikorvaukset")
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, items)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                when (position) {
-                    1 -> findNavController().navigate(R.id.action_start_to_paivaraha)
-                    2 -> findNavController().navigate(R.id.action_start_to_kilometrikorvaus)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // Do nothing
-            }
-        }
-        val recapButton: Button = view.findViewById(R.id.recap_btn)
-        recapButton.setOnClickListener {
-            recapClick(it)
+        val view = inflater.inflate(R.layout.fragment_yhteenveto, container, false)
+        val returnButton: Button = view.findViewById(R.id.return_btn)
+        returnButton.setOnClickListener {
+            returnClick(it)
         }
         return view
     }
 
-    fun recapClick(view: View){
-        findNavController().navigate(R.id.action_start_to_yhteenveto)
+    fun returnClick(view: View){
+        findNavController().navigate(R.id.action_yhteenveto_to_start)
     }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -78,17 +54,16 @@ class StartFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment StartFragment.
+         * @return A new instance of fragment YhteenvetoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            StartFragment().apply {
+            YhteenvetoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
 }
